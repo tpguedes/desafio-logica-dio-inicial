a923a100fe36732eb6388d3f3e0475e044929c45
Thiago Guedes <tguedes@gmail.com>
2024-08-12 20:30:07 -0300

    primeira versão

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log ("alterando");
+// Desafio classificador de nível de Herói
+
+let nomeHeroi = "Aghiot";
+let xpHeroi = 5672;
+let nivelHeroi;
+
+if (xpHeroi <= 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}.`);
+
